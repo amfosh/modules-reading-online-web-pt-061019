4a773a4c762617ce06692b5102a03beb0ca98e6f
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-20 21:30:05 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,13 +2,11 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  extend MetaDancing
-end
-  # include Dance 
+  include Dance 
   
-  # attr_accessor :name 
+  attr_accessor :name 
   
-  # def initialize(name)
-  #   @name = name 
-  # end
+  def initialize(name)
+    @name = name 
+  end
 end
